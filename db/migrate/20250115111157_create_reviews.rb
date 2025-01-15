e7b2d4cacd6e1b8659d4e6e8decf87a158c8f6c7
maxime nogues <maxime.nogues1408@gmail.com>
2025-01-15 12:29:19 +0100

    modifying the model

--- db/migrate/20250115111157_create_reviews.rb
+++ db/migrate/20250115111157_create_reviews.rb
@@ -2,7 +2,7 @@ class CreateReviews < ActiveRecord::Migration[7.1]
   def change
     create_table :reviews do |t|
       t.integer :rating
-      t.string :content
+      t.text :content
 
       t.timestamps
     end
